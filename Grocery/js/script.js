82aea1baa4ef5e6c595b989a1d9bfd9ce1a6e7e1
Ablaze99 <femiemma99@gmail.com>
2022-07-07 15:03:32 +0100

    grocery

--- Grocery/js/script.js
+++ Grocery/js/script.js
@@ -38,3 +38,50 @@ window.onscroll = () =>{
     loginForm.classList.remove('active')
     navBar.classList.remove('active');
 }
+
+      var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});
+
+    var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});
+
+ 
+     
+
+     
